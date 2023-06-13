3027f0fe94bdbceafa7d9bbf81b3b47403fd88ea
Jason Atallah <jasonatallah@gmail.com>
2023-06-13 08:54:55 -0700

    testing out packages

--- package-based-nx/apps/my-t3-app/src/pages/index.tsx
+++ package-based-nx/apps/my-t3-app/src/pages/index.tsx
@@ -1,11 +1,19 @@
 import { isEven } from "is-even";
 import { type NextPage } from "next";
 
+const Number = ({ num }: { num: number }) => {
+  return isEven(num) ? <span>{num} is even</span> : <span>{num} is odd</span>;
+};
 const Home: NextPage = () => {
   return (
     <div className="text-xl">
-      {isEven(2)}
-      {isEven(3)}
+      <p className="text-2xl">hello</p>
+      <p className="text-2xl">
+        <Number num={2} />
+      </p>
+      <p className="text-2xl">
+        <Number num={3} />
+      </p>
     </div>
   );
 };
